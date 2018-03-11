8e7ff26b8425698426e8626baee43467c3408d6e
Carol Xu <carolx715@gmail.com>
2018-03-10 21:22:42 -0500

    1st draft of all the rest of auto commands

---
+++ src/com/team4814/frc2018/autocommands/RightScaleAutoCommand.java
@@ -0,0 +1,45 @@
+package com.team4814.frc2018.autocommands;
+
+import com.team4814.frc2018.RobotConstants;
+import com.team4814.frc2018.commands.DrivePIDCommand;
+import com.team4814.frc2018.commands.MoveArmPIDCommand;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class RightScaleAutoCommand extends CommandGroup
+{
+
+	public RightScaleAutoCommand()
+	{
+		addSequential(new WaitForGameDataCommand());
+
+		addParallel(new MoveArmPIDCommand(RobotConstants.ARM_SCALE_HEIGHT));
+		addSequential(new DrivePIDCommand(80.0, 0.5)); // drive forward
+
+		while (true)
+		{
+			String message = DriverStation.getInstance().getGameSpecificMessage();
+			if (message != null && message.length() > 0)
+				break;
+
+			try
+			{
+				Thread.sleep(100);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		boolean goLeft = DriverStation.getInstance().getGameSpecificMessage().charAt(1) == 'L';
+		if (goLeft)
+			addSequential(new RightScaleLAutoCommand());
+		else
+			addSequential(new RightScaleRAutoCommand());
+	}
+}
