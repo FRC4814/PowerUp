6b056d447a1d8a02c5e7988d9bcfb10f98587659
carolx715 <carolx715@gmail.com>
2018-04-10 20:40:00 -0400

    Deleted unneeded auto stuff

--- src/com/team4814/frc2018/autocommands/AutoSlot3ToRightScale.java
+++ src/com/team4814/frc2018/autocommands/AutoSlot3ToRightScale.java
@@ -1,6 +1,8 @@
 package com.team4814.frc2018.autocommands;
 
+import com.team4814.frc2018.RobotConstants;
 import com.team4814.frc2018.commands.DriveBothPIDCommand;
+import com.team4814.frc2018.commands.MoveArmPIDCommand;
 import com.team4814.frc2018.commands.ToggleIntakeSolenoidCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -15,16 +17,24 @@ public class AutoSlot3ToRightScale extends CommandGroup
 	{
 		addParallel(new ToggleIntakeSolenoidCommand(true));
 
-		addSequential(new DriveBothPIDCommand(300.0, 300.0, 1.0));
+		addSequential(new DriveBothPIDCommand(304.0, 315.0, 1.0));
 
-		addSequential(new DriveBothPIDCommand(10, -10));
+		addSequential(new DriveBothPIDCommand(-25.0, 25.0, 0.8), 2.0); // rotate
 
-		addSequential(new AutoIntakeCommand(-1.0, -1.0), 1.0);
+		addParallel(new MoveArmPIDCommand(RobotConstants.ARM_SCALE_HEIGHT));
+
+		addSequential(new DriveBothPIDCommand(29.0, 29.0, 0.8), 1.0);
+
+		addSequential(new AutoIntakeCommand(-1.0, -1.0));
+
+		doWait();
+
+		addSequential(new AutoIntakeCommand(0.0, 0.0));
 
 	}
 
-	//	private void doWait()
-	//	{
-	//		addSequential(new WaitCommand(1.0));
-	//	}
+	private void doWait()
+	{
+		addSequential(new WaitCommand(1.0));
+	}
 }
