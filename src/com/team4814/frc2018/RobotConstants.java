027fd21dc7b01141afc39c2438f8b3b4dc60d17e
carolx715 <carolx715@gmail.com>
2018-04-10 20:14:06 -0400

    Western Competition ends

--- src/com/team4814/frc2018/RobotConstants.java
+++ src/com/team4814/frc2018/RobotConstants.java
@@ -8,30 +8,17 @@ public class RobotConstants
 	public static final double ARM_SWITCH_HEIGHT = -13.8325; // 45
 	public static final double ARM_SCALE_HEIGHT = 14.1675; // 93
 	public static final double ARM_DUMP_HEIGHT = 4.9175; // 83
-	//	public static final double ARM_DEFAULT_HEIGHT = -300.0;
-	//	public static final double ARM_HOLD_HEIGHT = -200.5825; // 15
-	//	public static final double ARM_SWITCH_HEIGHT = -100.8325; // 45
-	// new encoder values are 100 clicks per rotation instead of previous 160. Below are all previous values:
-	/*
-	 * Arm default: 0
-	 * Arm hold: 13.5
-	 * Arm switch: 43.5
-	 * Arm scale: 91.5
-	 * Arm dump: 81.5
-	 * Arm lift: 80.0
-	 */
-	// using bottom as zero instead of auto contraption:
-	/*
-	 * 0
-	 * 8.4375
-	 * 27.1875
-	 * 57.1875
-	 * 50.9375
-	 * 50
-	 */
 
 	// Auto constant
 	public static final double ARM_LIFT_HEIGHT = 7.98;
+	/* everything / 100
+	 * -0.4602
+	 * -0.375825
+	 * -0.138325
+	 * 0.141675
+	 * 0.049175
+	 * 0.0798
+	 */
 
 	// Intake
 	public static final double[] INTAKE_OUTAKE_DEFAULT_SPEED = { 0.0, 0.0 };
